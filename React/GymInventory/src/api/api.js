4fc8e86a2b208801a4d2693a8154e367fdb8e995
Piyushshind <piyushshinde.199@gmail.com>
2024-10-09 10:35:50 +0530

    getInventoryItemById created

--- React/GymInventory/src/api/api.js
+++ React/GymInventory/src/api/api.js
@@ -5,20 +5,30 @@ const API_URL = 'http://localhost:8071/api/v1/inventory';
 
 // Get all inventory items
 export const getInventoryItems = async () => {
-  return await axios.get(`${API_URL}`);
+    return await axios.get(`${API_URL}`);
 };
 
 // Add a new inventory item
 export const addInventoryItem = async (itemData) => {
-  return await axios.post(`${API_URL}/create`, itemData);
+    return await axios.post(`${API_URL}/create`, itemData);
+};
+
+// Update an inventory item
+// export const updateInventoryItem = async (id, updatedData) => {
+//   return await axios.put(`${API_URL}/update/${id}`, updatedData);
+// };
+
+// Delete an inventory item
+export const deleteInventoryItem = async (id) => {
+    return await axios.delete(`${API_URL}/delete/${id}`);
 };
 
 // Update an inventory item
 export const updateInventoryItem = async (id, updatedData) => {
-  return await axios.put(`${API_URL}/update/${id}`, updatedData);
+    return await axios.put(`${API_URL}/update/${id}`, updatedData);
 };
 
-// Delete an inventory item
-export const deleteInventoryItem = async (id) => {
-  return await axios.delete(`${API_URL}/delete/${id}`);
+// Get a single inventory item by ID
+export const getInventoryItemById = async (id) => {
+    return await axios.get(`${API_URL}/${id}`);
 };
