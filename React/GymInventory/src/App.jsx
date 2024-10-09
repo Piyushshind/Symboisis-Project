1dc93fe2cd979ff634cb30cf8d3d74780a527c41
Piyushshind <piyushshinde.199@gmail.com>
2024-10-09 10:41:29 +0530

    Routs defined

--- React/GymInventory/src/App.jsx
+++ React/GymInventory/src/App.jsx
@@ -1,35 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import react from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
+import InventoryList from './components/InventoryList';
+import AddInventoryForm from './components/AddInventoryForm';
+import UpdateInventoryForm from './components/UpdateInventoryForm';
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <div>
+          <Routes>
+            <Route path="/" element={<InventoryList />} />
+            <Route path="/add" element={<AddInventoryForm />} />
+            <Route path="/update/:id" element={<UpdateInventoryForm />} />
+          </Routes>
+        </div>
+      </Router>
     </>
   )
 }
 
-export default App
+export default App;
